dbe4c7d1386fbf1ac09281b3572b13d6f2198c52
Zan Drakslar <zdrakslar@gmail.com>
2018-06-26 13:02:33 +0200

    Add Comments Input Accessory View

--- InstagramClone/Comment/CommentsController.swift
+++ InstagramClone/Comment/CommentsController.swift
@@ -12,10 +12,71 @@ class CommentsController: UICollectionViewController {
     
     var post: Post?
     
+    var containerView: UIView = {
+        let containerView = UIView() //CustomView()
+        containerView.backgroundColor = .white
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 50) 
+        
+        let submitButton = UIButton(type: .system)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.setTitleColor(.black, for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
+        containerView.addSubview(submitButton)
+        submitButton.anchor(x: nil, y: nil, top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 0)
+        
+        let textField = UITextField()
+        textField.placeholder = "Enter Comment"
+        containerView.addSubview(textField)
+        textField.anchor(x: nil, y: nil, top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        return containerView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = .white
+        navigationItem.title = "Comments"
+        collectionView?.backgroundColor = .blue
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    override var inputAccessoryView: UIView? {
+        get {
+            return containerView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    
+    
+    @objc private func handleSubmit() {
+        print("...")
+    }
+    
+}
+
+class CustomView: UIView {
+    
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        
+        if #available(iOS 11.0, *) {
+            if let window = window {
+                bottomAnchor.constraintLessThanOrEqualToSystemSpacingBelow(window.safeAreaLayoutGuide.bottomAnchor, multiplier: 1).isActive = true
+            }
+        }
+    }
 }
